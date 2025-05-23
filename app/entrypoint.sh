f43adc18bb393e34a5702cfbe2334f7f3a5fa35d
Ivan Fomin <ivfmn1@gmail.com>
2025-05-23 20:56:30 +0400

    changed port bind to 8080

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py flush --no-input
 python manage.py migrate
 python manage.py collectstatic --no-input
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@email.com')" | python manage.py shell
-gunicorn app.wsgi:application --bind 0.0.0.0:8000
+gunicorn app.wsgi:application --bind 0.0.0.0:8080
 exec "$@"
