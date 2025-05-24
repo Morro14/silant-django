0c5e91552e5200ec3b9151fa742521ac7ee9e300
Ivan Fomin <ivfmn1@gmail.com>
2025-05-24 17:31:14 +0400

    entrypoint fix 5?

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # python manage.py flush --no-input
 # python manage.py migrate
+cd /app
 python manage.py collectstatic --no-input
 # echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@email.com')" | python manage.py shell
 gunicorn app.wsgi:application --bind 0.0.0.0:3001
